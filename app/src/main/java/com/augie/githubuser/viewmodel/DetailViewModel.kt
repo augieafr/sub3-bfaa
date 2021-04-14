e8428f3bb275b04df0cf57cd0a38d4b1bf692b72
augieafr <augieafriyansyah@gmail.com>
2021-04-15 00:07:20 +0700

    add detail user live data, set, and get function

--- app/src/main/java/com/augie/githubuser/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/augie/githubuser/viewmodel/DetailViewModel.kt
@@ -1,6 +1,18 @@
 package com.augie.githubuser.viewmodel
 
+import androidx.lifecycle.MutableLiveData
+import com.augie.githubuser.model.DetailUserModel
 import com.augie.githubuser.repository.DetailUserRepository
 
 class DetailViewModel(private val detailRepo: DetailUserRepository) {
+    val detailUser = MutableLiveData<DetailUserModel>()
+
+    fun setUserDetal(username: String?){
+        detailRepo.getUserDetail(username, detailUser)
+    }
+
+    fun getUserDetail(): MutableLiveData<DetailUserModel> {
+       return detailUser
+    }
+
 }
