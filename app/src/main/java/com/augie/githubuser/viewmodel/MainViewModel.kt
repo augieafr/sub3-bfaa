fa01f7e0ff8ef55e1f7150043db30bf1590ebdb9
augieafr <augieafriyansyah@gmail.com>
2021-04-14 23:31:40 +0700

    rename from UserViewModel to MainViewModel
    Because now this view model only for main activity

--- app/src/main/java/com/augie/githubuser/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/augie/githubuser/viewmodel/MainViewModel.kt
@@ -3,14 +3,17 @@ package com.augie.githubuser.viewmodel
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.augie.githubuser.BuildConfig
 import com.augie.githubuser.model.UserModel
+import com.augie.githubuser.repository.MainUserRepository
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import cz.msebera.android.httpclient.Header
 import org.json.JSONArray
-import org.json.JSONObject
+import java.lang.IllegalArgumentException
 
-class UserViewModel : ViewModel() {
+class MainViewModel(private val mainRepo: MainUserRepository) : ViewModel() {
     val listUser = MutableLiveData<ArrayList<UserModel>>()
 
     // function to set follower or following
@@ -18,7 +21,7 @@ class UserViewModel : ViewModel() {
         val listItem = ArrayList<UserModel>()
         val client = AsyncHttpClient()
         val url = "https://api.github.com/users/$username/$type"
-        val token = "token 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"
+        val token = BuildConfig.GITHUB_TOKEN
         client.addHeader("Authorization", token)
         client.addHeader("User-Agent", "request")
         client.get(url, object : AsyncHttpResponseHandler(){
@@ -56,49 +59,20 @@ class UserViewModel : ViewModel() {
     }
 
     fun setUser(username: String?){
-        val listItem = ArrayList<UserModel>()
-        val client = AsyncHttpClient()
-        val url = "https://api.github.com/search/users?q=$username"
-        val token = "token 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"
-        client.addHeader("Authorization", token)
-        client.addHeader("User-Agent", "request")
-        client.get(url, object: AsyncHttpResponseHandler(){
-            override fun onSuccess(
-                statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray
-            ) {
-                val result = String(responseBody)
-                try {
-                    val responseObject = JSONObject(result)
-                    val list = responseObject.getJSONArray("items")
-
-                    for (i in 0 until list.length()){
-                        val user = list.getJSONObject(i)
-                        val searchResult = UserModel()
-                        searchResult.name = user.getString("login")
-                        searchResult.photo = user.getString("avatar_url")
-                        listItem.add(searchResult)
-                    }
-                    listUser.postValue(listItem)
-                } catch (e: Exception){
-                    e.printStackTrace()
-                }
-            }
-
-            override fun onFailure(
-                statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray?,
-                error: Throwable?
-            ) {
-                Log.d("onFailure", error?.message.toString())
-            }
-
-        })
+        mainRepo.getSearchUser(username, listUser)
     }
 
     fun getUser(): MutableLiveData<ArrayList<UserModel>>{
         return listUser
+    }
+}
+
+class MainViewModelFactory(private val mainRepo: MainUserRepository) : ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        @Suppress("UNCHECKED_CAST")
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)){
+            return MainViewModel(mainRepo) as T
+        }
+        throw IllegalArgumentException("Unknown view model class")
     }
 }
