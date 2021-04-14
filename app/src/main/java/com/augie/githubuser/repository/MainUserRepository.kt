bedf3ffebe0619071101677a5701d948e437a559
augieafr <augieafriyansyah@gmail.com>
2021-04-14 23:36:36 +0700

    create MainUserRepository

---
+++ app/src/main/java/com/augie/githubuser/repository/MainUserRepository.kt
@@ -0,0 +1,14 @@
+package com.augie.githubuser.repository
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.augie.githubuser.BuildConfig
+import com.augie.githubuser.model.UserModel
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
+
+class MainUserRepository {
+
+}
