0085dba163d95c31bb5ba5eb789a2bbb9eaad7b5
augieafr <augieafriyansyah@gmail.com>
2021-04-14 23:59:25 +0700

    add data class property

--- app/src/main/java/com/augie/githubuser/model/DetailUserModel.kt
+++ app/src/main/java/com/augie/githubuser/model/DetailUserModel.kt
@@ -1,5 +1,12 @@
 package com.augie.githubuser.model
 
-class DetailUserModel {
-
-}
+data class DetailUserModel (
+    var name: String = "",
+    var email: String = "",
+    var location: String = "",
+    var company: String = "",
+    var follower: Int = 0,
+    var following: Int = 0,
+    var repository: Int = 0,
+    var photo: String = ""
+)
